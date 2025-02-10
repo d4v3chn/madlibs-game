dfd206e32e24fb3064748404790932ac4615e322
d4v3ch <datochunti@gmail.com>
2025-02-10 22:50:08 +0400

    Finalizing...

--- src/MadLibs.java
+++ src/MadLibs.java
@@ -1,8 +1,13 @@
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.PlainDocument;
+
 import java.awt.Font;
 import javax.swing.JButton;
 import javax.swing.GroupLayout;
@@ -12,13 +17,14 @@ import javax.swing.LayoutStyle.ComponentPlacement;
 import java.awt.SystemColor;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
 
 public class MadLibs extends JFrame {
 	private JTextField adjTxt;
 	private JTextField colorTxt;
 	private JTextField verbTxt;
 	private JTextField nounTxt;
-	private JLabel mainTxtVar;
+	private JTextArea mainTxtVar;
 	
 	public void getMadLibs() {
 		String adjective = adjTxt.getText();
@@ -28,16 +34,15 @@ public class MadLibs extends JFrame {
 		
 		String text = "The " + color +" dragon " + verb +" at the " +
 				adjective + " " + noun;
-		text += '\n' + "And everyone lived happily ever after.";
-		text += '\n' + "The end.";
+		text += "\nAnd everyone lived happily ever after.";
+		text += "\nThe end.";
 		
 		mainTxtVar.setText(text);
-		
-		
 	}
 	
 	
 	public MadLibs() {
+		setResizable(false);
 		setTitle("Mad Libs");
 		
 		JLabel lblNewLabel = new JLabel("Wacky Shmacky Zahaha Mad Libs");
@@ -62,6 +67,11 @@ public class MadLibs extends JFrame {
 		JLabel lblNewLabel_2_1 = new JLabel("Enter a Noun:");
 		
 		nounTxt = new JTextField();
+		nounTxt.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				getMadLibs();
+			}
+		});
 		nounTxt.setColumns(10);
 		
 		JButton goButton = new JButton("Press here to view your Mad Libs monstrosity");
@@ -71,20 +81,19 @@ public class MadLibs extends JFrame {
 			}
 		});
 		
-		mainTxtVar = new JLabel("");
-		mainTxtVar.setForeground(SystemColor.inactiveCaption);
-		mainTxtVar.setFont(new Font("Vladimir Script", Font.BOLD, 15));
-		mainTxtVar.setHorizontalAlignment(SwingConstants.CENTER);
+		mainTxtVar = new JTextArea();
+		mainTxtVar.setEditable(false);
+		mainTxtVar.setFont(new Font("Lucida Handwriting", Font.ITALIC, 18));
 		GroupLayout groupLayout = new GroupLayout(getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
 					.addGap(126)
-					.addComponent(goButton, GroupLayout.DEFAULT_SIZE, 332, Short.MAX_VALUE)
+					.addComponent(goButton, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
 					.addGap(126))
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
-						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
+						.addGroup(groupLayout.createSequentialGroup()
 							.addGap(50)
 							.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
 								.addGroup(groupLayout.createSequentialGroup()
@@ -103,14 +112,14 @@ public class MadLibs extends JFrame {
 									.addComponent(lblNewLabel_2)
 									.addGap(18)
 									.addComponent(colorTxt, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))))
-						.addGroup(Alignment.LEADING, groupLayout.createSequentialGroup()
+						.addGroup(groupLayout.createSequentialGroup()
 							.addGap(16)
 							.addComponent(lblNewLabel)))
 					.addContainerGap(16, Short.MAX_VALUE))
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(14)
-					.addComponent(mainTxtVar, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-					.addGap(14))
+					.addGap(22)
+					.addComponent(mainTxtVar, GroupLayout.DEFAULT_SIZE, 541, Short.MAX_VALUE)
+					.addGap(21))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
@@ -139,15 +148,37 @@ public class MadLibs extends JFrame {
 						.addComponent(nounTxt, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
 					.addGap(31)
 					.addComponent(goButton)
-					.addGap(18)
-					.addComponent(mainTxtVar, GroupLayout.PREFERRED_SIZE, 86, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(21, Short.MAX_VALUE))
+					.addPreferredGap(ComponentPlacement.RELATED, 18, Short.MAX_VALUE)
+					.addComponent(mainTxtVar, GroupLayout.PREFERRED_SIZE, 82, GroupLayout.PREFERRED_SIZE)
+					.addGap(25))
 		);
 		getContentPane().setLayout(groupLayout);
 	}
+	
+//	Text Limit
+//	private void setCharLimit(JTextField tf, int limit) {
+//		PlainDocument doc = (PlainDocument) tf.getDocument();
+//		doc.setDocumentFilter(new DocumentFilter() {
+//			@Override
+//            public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+//                if (fb.getDocument().getLength() + string.length() <= limit) {
+//                    super.insertString(fb, offset, string, attr);
+//                }
+//            }
+//
+//            @Override
+//            public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+//                if (fb.getDocument().getLength() + text.length() - length <= limit) {
+//                    super.replace(fb, offset, length, text, attrs);
+//            }
+//		});
+//	}
 
 	public static void main(String[] args) {
+		MadLibs theGame = new MadLibs();
+		theGame.setSize(new Dimension(600,400));
+		theGame.setVisible(true);
+		theGame.setLocationRelativeTo(null);
 		
-
 	}
 }
